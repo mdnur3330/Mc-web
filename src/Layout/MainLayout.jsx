6c5a5d2a0528ffe8e98ad194ee25fe0094b11dfa
Md Nur Alom <hmnuralomseddeki@gmail.com>
2025-11-02 19:20:08 +0600

    readd banner

--- src/Layout/MainLayout.jsx
+++ src/Layout/MainLayout.jsx
@@ -1,15 +1,15 @@
-import Footer from '../Component/footer';
-import Nav from '../Component/Nav';
-import { Outlet } from 'react-router';
+import Footer from "../Component/footer";
+import Nav from "../Component/Nav";
+import { Outlet } from "react-router";
 
 const MainLayout = () => {
-    return (
-        <div className='bg-[#EEF3F6] pt-5 md:pt-7'>
-            <Nav />
-            <Outlet />
-            <Footer />
-        </div>
-    );
+  return (
+    <div className="bg-[#EEF3F6] pt-5 md:pt-7">
+      <Nav />
+      <Outlet />
+      <Footer />
+    </div>
+  );
 };
 
 export default MainLayout;
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,23 +1,23 @@
-import Banner from '../Component/Banner';
-import CommunitySays from '../Component/CommunitySays';
-import CompanyLogo from '../Component/CompanyLogo';
-import Contact from '../Component/Contact';
-import SkillDevelopmentPrograms from '../Component/SkillDevelopmentPrograms';
-import TrustedByLearners from '../Component/TrustedByLearners';
-import UpcomingEvents from '../Component/UpcomingEvents';
+import Banner from "../Component/Banner";
+import CommunitySays from "../Component/CommunitySays";
+import CompanyLogo from "../Component/CompanyLogo";
+import Contact from "../Component/Contact";
+import SkillDevelopmentPrograms from "../Component/SkillDevelopmentPrograms";
+import TrustedByLearners from "../Component/TrustedByLearners";
+import UpcomingEvents from "../Component/UpcomingEvents";
 
 const Home = () => {
-    return (
-        <div className='space-y-20'>
-            <Banner />
-            <CompanyLogo />
-            <SkillDevelopmentPrograms />
-            <UpcomingEvents />
-            <TrustedByLearners />
-            <CommunitySays />
-            <Contact />
-        </div>
-    );
+  return (
+    <div className="space-y-20">
+      <Banner />
+      <CompanyLogo />
+      <SkillDevelopmentPrograms />
+      <UpcomingEvents />
+      <TrustedByLearners />
+      <CommunitySays />
+      <Contact />
+    </div>
+  );
 };
 
 export default Home;
